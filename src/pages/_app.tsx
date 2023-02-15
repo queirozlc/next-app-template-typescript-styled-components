3baa007a9675ce242b873d08c7b077adb5e0286c
Lucas :) <91512054+queirozlc@users.noreply.github.com>
2023-02-14 22:12:11 -0300

    init: initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import GlobalStyle from '@/styles/global'
+import { ThemeProvider } from 'styled-components'
+import theme from '@/styles/themes/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
 }
